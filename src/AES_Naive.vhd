34c51b088be5f0ac246848eb3aefb84200b97b59
petr <petr@lambda>
2020-07-21 21:42:27 +0200

    Add license

--- src/AES_Naive.vhd
+++ src/AES_Naive.vhd
@@ -24,10 +24,12 @@ end entity AES_Naive;
 architecture arch of AES_Naive is
   signal pre_state : block_t := (others => '0');
   signal state     : state_t := (others => (others => (others => '0')));
-  signal state_sb  : state_t;
-  signal state_sr  : state_t;
-  signal state_mc  : state_t;
+  signal state_sb  : state_t := (others => (others => (others => '0')));
+  signal state_sr  : state_t := (others => (others => (others => '0')));
+  signal state_mc  : state_t := (others => (others => (others => '0')));
 begin
+
+  
   process (input, key, pre_state, state, state_sb, state_sr) is
   begin
     pre_state <= input xor key;
@@ -36,5 +38,7 @@ begin
     state_sr  <= shift_rows(state_sb);
     state_mc  <= mix_columns(state_sr);
   end process;
+
+  output <= (others => '0');
 
 end architecture arch;
